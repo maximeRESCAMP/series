3c88104e70b0d389de36098511efdfd4da41810c
mrescamp2022 <maxime.rescamp@hotmai.fr>
2023-02-21 12:24:57 +0100

    ajout de la base html.twig + block body

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,13 @@ class MainController extends AbstractController
     #[Route('/home', name: 'main_home')]
     public function home(): Response
     {
-   return $this->render("main/home.html.twig");
+        $username = '<h1>Sylvain</h1>';
+        $serie = [ 'title' => 'Community', 'year '=>'Ouf', 'plateform'=>'NBC'];
+   return $this->render("main/home.html.twig",[
+       //la clé devien le nom de la variable cote twig
+       "name" => $username,
+       "serie"=> $serie
+       ]);
     }
 
     /**
